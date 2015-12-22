6a7391b95992dc6acb9e5a63d68ab56807e74c8d
Rod <supgeek.rod@gmail.com>
2015-12-22 16:49:53 +0800

    add infinite refresh and pull refresh

--- www/js/services/activity.service.js
+++ www/js/services/activity.service.js
@@ -2,6 +2,6 @@ HeyCommunity
 
 .service('ActivityService', function($http) {
     this.index = function() {
-        return $http.get('/activity');
+        return $http.get(getApiUrl('/activity'));
     }
 })

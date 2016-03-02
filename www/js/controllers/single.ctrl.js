d6be0742c7cbb1908c002b572ba5eddea6f2ae53
SupeRodv2 <SupeRodv2@SupeRods-MacBook.local>
2016-03-02 13:46:43 +0800

    the debug state

--- www/js/controllers/single.ctrl.js
+++ www/js/controllers/single.ctrl.js
@@ -4,6 +4,11 @@ HeyCommunity
 .controller('SingleDevingCtrl', ['$scope',  function($scope) {
     $scope.localStorage = localStorage;
 
+    console.log(navigator)
+    $scope.env = {
+        navigatorLanguage: navigator.language,
+    };
+
     $scope.destroyLocalStorage = function() {
         localStorage.clear();
         console.log(localStorage);

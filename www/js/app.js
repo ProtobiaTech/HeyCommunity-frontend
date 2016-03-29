cd20ba751c97cc5bb0b121432450c5a90c9b49eb
SupeRodv2 <supgeek.rod@gmail.com>
2016-03-29 20:49:49 +0800

    migration filter

--- www/js/app.js
+++ www/js/app.js
@@ -121,17 +121,3 @@ var HeyCommunity = angular.module('starter', [
         }
     }])
 }])
-
-
-.filter('nl2br', ['$sce', function ($sce) {
-    return function (text) {
-        text = text ? text.replace(/\n/g, '<br>') : '';
-        return $sce.trustAsHtml(text);
-    };
-}])
-
-.filter('momentDate', ['$sce', function ($sce) {
-    return function (date) {
-        return moment.utc(date).format();
-    };
-}]);

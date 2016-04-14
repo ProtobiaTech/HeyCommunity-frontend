a3a56ab0ca509e23b03b9142d3b6da735e6395ad
SupeRodv2 <supgeek.rod@gmail.com>
2016-04-14 15:06:19 +0800

    the user signIn

--- www/js/services/userSignIn.service.js
+++ www/js/services/userSignIn.service.js
@@ -15,11 +15,6 @@ HeyCommunity
                 $rootScope.userInfo = response.data;
 
                 $rootScope.signInModal.hide();
-                if ($rootScope.jumpRoute) {
-                    $rootScope.state.go($rootScope.jumpRoute);
-                } else {
-                    $rootScope.state.go('hey.user');
-                }
             } else {
                 var content = $rootScope.filter('translate')('PHONE_OR_PASSWORD_ERROR');
                 $rootScope.utility.showAlert({title: $rootScope.filter('translate')('ERROR'), content: content});

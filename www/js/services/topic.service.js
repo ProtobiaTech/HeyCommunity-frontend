f626d8bd2e2f198f9162304ea5315fa941c97592
SupeRodv2 <supgeek.rod@gmail.com>
2016-04-19 19:26:29 +0800

    the topics

--- www/js/services/topic.service.js
+++ www/js/services/topic.service.js
@@ -1,6 +1,6 @@
 HeyCommunity
 
-.service('TopicService', ['$http', function($http) {
+.service('TopicService', ['$http', '$rootScope', 'UtilityService',  function($http, $rootScope, UtilityService) {
     //
     //
     var self = this;
@@ -8,29 +8,43 @@ HeyCommunity
 
 
     //
+    //
+    self.saveInLocalStorage = function() {
+        localStorage.topics = JSON.stringify(self.topics);
+    }
+
+
+    //
+    //
+    self.getByLocalStorage = function() {
+        if (localStorage.topics) {
+            self.topics = JSON.parse(localStorage.topics);
+        }
+    }
+
+
+    //
     // index
     self.index = function(params) {
         var url = getApiUrl('/topic');
-        if (typeof(params) == 'object' && 'type' in params) {
-            url = url + '?type=' + params.type + '&id=' + params.id;
+        if (typeof(params) == 'object' && 'type' in params && self.topics.length > 0) {
+            if (params.type === 'refresh') {
+                var id = $rootScope.filter('orderBy')(self.topics, '-id')[0].id;
+                url = url + '?type=' + params.type + '&id=' + id;
+            } else if (params.type === 'infinite') {
+                var id = $rootScope.filter('orderBy')(self.topics, 'id')[0].id;
+                url = url + '?type=' + params.type + '&id=' + id;
+            }
         }
 
         var q = $http.get(url);
         q.then(function(response) {
             if (response.status == 200) {
-                if (params !== undefined) {
-                    if (params.type === 'refresh') {
-                        while (response.data.length > 0) {
-                            self.topics.unshift(response.data.shift());
-                        }
-                    } else if (params.type === 'infinite') {
-                        self.topics = self.topics.concat(response.data);
-                    }
-                } else {
-                    self.topics = response.data;
-                }
+                self.topics = self.topics.concat(response.data);
 
-                localStorage.topics = JSON.stringify(self.topics);
+                self.saveInLocalStorage();
+            } else {
+                UtilityService.showNoticeFail();
             }
         })
 
@@ -41,30 +55,82 @@ HeyCommunity
     //
     // store
     self.store = function(http, params) {
-        return http.upload({
+        var q = http.upload({
             url: getApiUrl('/topic/store'),
             data: params
         });
+        q.then(function(response) {
+            //
+        }, function() {
+            UtilityService.showNoticeFail();
+        })
+
+        return q;
     }
 
 
     //
     // destory
     self.destroy = function(params) {
-        return $http.post(getApiUrl('/topic/destroy'), params);
+        var q = $http.post(getApiUrl('/topic/destroy'), params);
+        q.then(function(response) {
+            if (response.status == 200) {
+                angular.forEach(self.topics, function(value, key) {
+                    if (value.id === params.id) {
+                        delete self.topics.splice(key, 1);
+                    }
+                });
+
+                self.saveInLocalStorage();
+            }
+        }, function() {
+            UtilityService.showNoticeFail();
+        })
+
+        return q;
     }
 
 
     //
     // show
     self.show = function(params) {
-        return $http.get(getApiUrl('/topic/show/' + params.id));
+        var q = $http.get(getApiUrl('/topic/show/' + params.id));
+        q.then(function(response) {
+            if (response.status === 200) {
+                angular.forEach(self.topics, function(value, key) {
+                    if (value.id === response.data.id) {
+                        self.topics[key] = response.data;
+                    }
+                });
+
+                self.saveInLocalStorage();
+            }
+        }, function() {
+            UtilityService.showNoticeFail();
+        })
+
+        return q;
     }
 
 
     //
     // comment
     self.commentPublish = function(params) {
-        return $http.post(getApiUrl('/topic/comment-publish'),  params);
+        var q = $http.post(getApiUrl('/topic/comment-publish'),  params);
+        q.then(function(response) {
+            if (response.status === 200) {
+                angular.forEach(self.topics, function(value, key) {
+                    if (value.id === response.data.id) {
+                        self.topics[key] = response.data;
+                    }
+                });
+
+                self.saveInLocalStorage();
+            }
+        }, function() {
+            UtilityService.showNoticeFail();
+        })
+
+        return q;
     }
 }])

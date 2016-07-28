e103b98257a6416f7f112cd8dae89f55ce366060
SupeRodv2 <supgeek.rod@gmail.com>
2016-07-28 13:26:48 +0800

    modify moment piper

--- app/other/moment.pipe.ts
+++ app/other/moment.pipe.ts
@@ -7,10 +7,11 @@ import * as moment from 'moment';
     pure: false,
 })
 export class MomentPipe implements PipeTransform {
-    transform(d:Date | moment.Moment, args?:any[]):string {
-        let rv = moment(d).format(args[0]);
-        return rv;
-    }
+  transform(d:Date | moment.Moment, args?:any[]):string {
+    // utc add 8 hours into beijing
+    let rv = moment(d).add(8, 'hours').format(args[0]);
+    return rv;
+  }
 }
 
 @Pipe({
@@ -18,8 +19,9 @@ export class MomentPipe implements PipeTransform {
     pure: false,
 })
 export class TimeagoPipe implements PipeTransform {
-    transform(d:Date | moment.Moment):string {
-        let rv = moment(d).fromNow();
-        return rv;
-    }
+  transform(d:Date | moment.Moment):string {
+    // utc add 8 hours into beijing
+    let rv = moment(d).add(8, 'hours').fromNow();
+    return rv;
+  }
 }

634e96952d55fb22e480d5092aae0598ffeea4c3
SupeRodv2 <supgeek.rod@gmail.com>
2016-08-03 16:32:42 +0800

    the menu

--- app/app.ts
+++ app/app.ts
@@ -1,20 +1,47 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, ionicBootstrap, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {Helper} from './other/helper.component';
 
 import {TabsPage} from './pages/tabs/tabs';
+import {TimelinePage} from './pages/timeline/timeline';
 
+
+interface PageObj {
+  icon: string;
+  title: string;
+  component?: any;
+  index?: number;
+}
 
 @Component({
   templateUrl: 'build/app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
 
   private rootPage:any;
 
-  constructor(private platform:Platform) {
+  appPages: PageObj[] = [
+    {icon: 'pulse', title: 'Timeline', component: TabsPage, index: 0},
+    {icon: 'images', title: 'Timeline', component: TabsPage, index: 1},
+    {icon: 'flower', title: 'Timeline', component: TabsPage, index: 2},
+    {icon: 'sunny', title: 'Timeline', component: TabsPage, index: 3},
+    {icon: 'aperture', title: 'Timeline', component: TabsPage, index: 4},
+  ]
+
+  loggedOutPages: [Object] = [
+    {icon: 'log-in', title: 'Login'},
+    {icon: 'log-out', title: 'Logout'},
+  ]
+
+
+  //
+  //
+  constructor(
+    private platform:Platform
+  ) {
     this.rootPage = TabsPage;
 
     platform.ready().then(() => {
@@ -23,7 +50,29 @@ export class MyApp {
       StatusBar.styleDefault();
     });
   }
+
+
+  //
+  //
+  openPage(page: PageObj) {
+    if (page.index) {
+      this.nav.setRoot(page.component, {tabIndex: page.index});
+
+    } else {
+      this.nav.setRoot(page.component);
+    }
+
+    /*
+    if (page.title === 'Logout') {
+      // Give the menu time to close before changing to logged out
+      setTimeout(() => {
+        this.userData.logout();
+      }, 1000);
+    }
+    */
+  }
 }
+
 
 ionicBootstrap(MyApp, [
   HTTP_PROVIDERS,
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,24 +1,31 @@
 import {Component} from '@angular/core';
+import {NavParams} from 'ionic-angular';
+
 import {TimelinePage} from '../timeline/timeline';
 import {TopicPage} from '../topic/topic';
 import {HomePage} from '../home/home';
 import {AboutPage} from '../about/about';
 import {ContactPage} from '../contact/contact';
 
+
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
 })
 export class TabsPage {
+  mySelectedIndex: number;
 
-  private tab1Root: any;
-  private tab2Root: any;
-  private tab3Root: any;
+  tab1Root: any = TimelinePage;
+  tab2Root: any = TopicPage;
+  tab3Root: any = ContactPage;
 
-  private homeRoot: any;
-  private aboutRoot: any;
-  private contactRoot: any;
+  homeRoot: any = HomePage;
+  aboutRoot: any = AboutPage;
+  contactRoot: any = ContactPage;
 
-  constructor() {
+
+  //
+  //
+  constructor(navParams: NavParams) {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = TimelinePage;
@@ -28,5 +35,7 @@ export class TabsPage {
     this.homeRoot = HomePage;
     this.aboutRoot = AboutPage;
     this.contactRoot = ContactPage;
+
+    this.mySelectedIndex = navParams.data.tabIndex || 0;
   }
 }

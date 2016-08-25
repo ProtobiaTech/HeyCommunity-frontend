cf512eaaf61bf7dca9e9067ae58b73030b87aff1
SupeRodv2 <supgeek.rod@gmail.com>
2016-08-25 11:55:02 +0800

    the me-update-profile page

--- app/pages/me/me-updateProfile.ts
+++ app/pages/me/me-updateProfile.ts
@@ -1,25 +1,28 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, NavParams} from 'ionic-angular';
 
 import {Helper} from '../../other/helper.component';
 import {Auth} from '../../other/auth.component';
 import {UserService} from '../../services/user.service';
 
-import {MeUpdateProfilePage} from '../../pages/me/me-updateProfile';
-
 
 @Component({
-  templateUrl: 'build/pages/me/me-profile.html'
+  templateUrl: 'build/pages/me/me-updateProfile.html'
 })
-export class MeProfilePage {
-  meUpdateProfilePage = MeUpdateProfilePage;
+export class MeUpdateProfilePage {
+  //
+  item: string;
 
 
+  //
+  //
   constructor(
     private navCtrl: NavController,
+    private navParams: NavParams,
     private auth: Auth,
     private userService: UserService
   ) {
+    this.item = this.navParams.get('item');
   }
 
 

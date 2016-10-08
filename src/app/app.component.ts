c86901a26cca720d9dedde60c142a613c5ab42db
SupeRodv2 <supgeek.rod@gmail.com>
2016-10-08 16:31:12 +0800

    restore src/assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { TutorialPage } from '../pages/tutorial/tutorial';
+// import { TutorialPage } from '../pages/tutorial/tutorial';
 
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  // rootPage = TabsPage;
-  rootPage = TutorialPage;
+  rootPage = TabsPage;
+  // rootPage = TutorialPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {

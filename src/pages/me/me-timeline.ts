4351c338cd86673d5e990f8216a15aab4f54198c
SupeRodv2 <supgeek.rod@gmail.com>
2016-10-12 19:01:37 +0800

    the me timeline

--- src/pages/me/me-timeline.ts
+++ src/pages/me/me-timeline.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Helper } from '../../other/helper';
+import { AuthenticateComponent } from '../../pages/component/authenticate';
+import { UtilityComponent } from '../../pages/component/utility';
+
+import { TimelineService } from '../../services/timeline.service';
+import { AuthenticateService } from '../../services/authenticate.service';
+
 import { Timeline } from '../../models/timeline.model';
+import { TimelineDetailPage } from '../../pages/timeline/timeline-detail';
 
 
 @Component({
@@ -14,8 +22,44 @@ export class MeTimelinePage {
 
   //
   // constructor
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public helper: Helper,
+    public utilityComp: UtilityComponent,
+    public authComp: AuthenticateComponent,
+    public timelineService: TimelineService,
+    public authService: AuthenticateService,
+    public navCtrl: NavController
+  ) {
     this.timelines = [];
+
+    this.timelineService.timelines.forEach(function(timeline) {
+      if (timeline.user_id === this.authService.userInfo.id) {
+        this.timelines = this.timelines.concat(timeline);
+      }
+    }, this)
   }
+
+
+  //
+  // set like for timeline
+  setLikeForTimeline(timeline) {
+    if (this.authService.isAuth) {
+      this.timelineService.setLike(timeline)
+      .then(newTimeline => {
+        timeline.is_like = newTimeline.is_like;
+        timeline.like_num = newTimeline.like_num;
+      });
+    } else {
+      this.authComp.presentAuthModal();
+    }
+  }
+
+
+  //
+  // goto timeline detail page
+  gotoTimelineDetailPage(timeline, index) {
+    this.navCtrl.push(TimelineDetailPage, {timeline: timeline, timelineIndex: index});
+  }
+
 
 }

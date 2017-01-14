56a26f45e8476b93e6557bd6d01269b7036ddbed
supeRodv2 <supgeek.rod@gmail.com>
2017-01-14 13:53:16 +0800

    for ionic cloud

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
 
-gulp.task('lpush-install', function(done){
+
+/*
+//
+gulp.task('build:before', ['lpush-deploy']);
+gulp.task('upload:before', ['lpush-deploy']);
+
+
+//
+gulp.task('lpush-deploy', function(done){
     require('./plugins/cordova-plugin-leanpush/lpush-installer.js')(__dirname, done);
 });
+*/

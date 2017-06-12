58861e0538f99ba83033ce32649ad5df385ecf89
supeRodv2 <supgeek.rod@gmail.com>
2017-06-12 21:10:19 +0800

    收藏集页面

--- src/modules/collect/pages/collect.ts
+++ src/modules/collect/pages/collect.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { AppService } from '../../common/services/app.service';
+import { TimelineService } from '../../timeline/services/timeline.service';
 
 import { CollectEditPage } from '../../collect/pages/collect-edit';
 import { CollectMemberPage } from '../../collect/pages/collect-member';
@@ -15,9 +16,18 @@ export class CollectPage {
   //
   // constructor
   constructor(
+    public timelineService: TimelineService,
     public heyApp: AppService,
     public navCtrl: NavController
   ) {
+  }
+
+
+  //
+  // ion view did enter
+  ionViewDidEnter() {
+    // get timelines
+    this.timelineService.index();
   }
 
 
